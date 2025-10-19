d7a9179a704ad37b5f73cb729b26ead9b737c3ef
Paul <paul@pvt.design>
2025-10-19 16:46:17 -0700

    Search routes, filters, and fetching gifts

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -11,25 +11,24 @@ router.get('/', async (req, res, next) => {
         // Initialize the query object
         let query = {};
 
-        // Add the name filter to the query if the name parameter is not empty
-        // if (/* {{insert code here}} */) {
-        query.name = { $regex: req.query.name, $options: 'i' }; // Using regex for partial match, case-insensitive
-        // }
+        // Searching by name, return result if name is provided and not empty
+        if (req.query.name.trim() !== '') {
+            query.name = { $regex: req.query.name, $options: 'i' }; // Using regex for partial match, case-insensitive
+        }
 
-        // Task 3: Add other filters to the query
+        // Other filters
         if (req.query.category) {
-            // {{insert code here}}
+            query.category = req.query.category;
         }
         if (req.query.condition) {
-            // {{insert code here}}
+            query.condition = req.query.condition;
         }
         if (req.query.age_years) {
-            // {{insert code here}}
             query.age_years = { $lte: parseInt(req.query.age_years) };
         }
 
-        // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
-        // {{insert code here here}}
+        // Fetch filtered gifts
+        await collection.find(collection).toArray();
 
         res.json(gifts);
     } catch (e) {
