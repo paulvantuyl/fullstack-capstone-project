e57e8af9127359078291e690daecf47f55d1c2fd
Paul <paulvantuyl@users.noreply.github.com>
2025-11-02 18:00:52 -0700

    Update product routing

--- giftlink-frontend/src/components/MainPage/MainPage.js
+++ giftlink-frontend/src/components/MainPage/MainPage.js
@@ -27,7 +27,7 @@ function MainPage() {
     // Task 2: Navigate to details page
     const goToDetailsPage = (productId) => {
         // Write your code below this line
-        navigate(`/gifts/${productId}`);
+        navigate(`/app/product/${productId}`);
       };
 
     // Task 3: Format timestamp
