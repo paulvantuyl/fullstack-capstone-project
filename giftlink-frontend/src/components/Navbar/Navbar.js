3e494173fbcce6129caad11e9db36c8f19716704
Paul <paulvantuyl@users.noreply.github.com>
2025-11-02 19:25:51 -0700

    Updates to navbar

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -3,18 +3,22 @@ import React from 'react';
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <a className="navbar-brand" href="/">GiftLink</a>
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">GiftLink</a>
 
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    {/* Task 1: Add links to Home and Gifts below*/}
-                    <li className="nav-item">
-                        <a className="nav-link" href="/home.html">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/app">Gifts</a>
-                    </li>
-                </ul>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"></button>
+
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        {/* Task 1: Add links to Home and Gifts below*/}
+                        <li className="nav-item">
+                            <a className="nav-link" href="/home.html">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="/app">Gifts</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
