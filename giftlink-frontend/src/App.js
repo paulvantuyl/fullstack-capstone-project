900543620c9648ba1a0d79450e3dd12fab31476b
Paul <paulvantuyl@users.noreply.github.com>
2025-11-08 11:58:01 -0700

    Add product details page to routes

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
 import MainPage from './components/MainPage/MainPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import LoginPage from './components/LoginPage/LoginPage';
+import Navbar from './components/Navbar/Navbar';
+import DetailsPage from './components/DetailsPage/DetailsPage';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
@@ -17,6 +20,7 @@ function App() {
         <Route path="/app" element={<MainPage />} />
         <Route path="/app/register" element={<RegisterPage />} />
         <Route path="/app/login" element={<LoginPage />} />
+        <Route path="/app/product/:id" element={<DetailsPage />} />
       </Routes>
   </>
   );
