54fba4df11d9fcbd7416dbbd4f749fdcde45f1b4
Paul <paulvantuyl@users.noreply.github.com>
2025-12-04 07:19:26 -0700

    Remove dependencies no longer being used

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -5,8 +5,6 @@ import { useAppContext } from '../../context/AuthContext';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 
 export default function NavbarComponent() {
     const navigate = useNavigate();
